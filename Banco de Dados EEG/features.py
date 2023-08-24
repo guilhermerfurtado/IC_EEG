cd7d76d241b37345ec751c5127a98e32e756ebfd
guilhermerfurtado <132095325+guilhermerfurtado@users.noreply.github.com>
2023-08-24 13:45:40 -0300

    Update features.py

--- Banco de Dados EEG/features.py
+++ Banco de Dados EEG/features.py
@@ -8,6 +8,15 @@ import os
 
 lista_arquivos = [i for i in os.listdir('./filtrados') if i.find('.csv') != -1]
 
+mav = []  # mean absolute value
+rms = []  # root mean square
+zcs = []  # zero crossing
+scc = []  # slope sign changes
+wal = []  # waveform length
+
+var = []  # variance
+fmn = []  # frequency mean
+fmd = []  # frequency median
 
 for nome_arquivo in lista_arquivos:
 
@@ -17,15 +26,24 @@ for nome_arquivo in lista_arquivos:
     channels = list(leitor_csv.head())[:-1]
 
     times = [8, 23, 39, 52]
-    intervalo_inicio = 8
-    intervalo_meio1 = 24
-    intervalo_meio2 = 40
-    intervalo_fim = 52
 
     for canal in channels:
         for i in range(3): 
             dados = leitor_csv[(leitor_csv['Tempo'] >= times[i]) & (leitor_csv['Tempo'] <= times[i+1])]
-            print(len(dados))
-        
-        print('-----------------')
+
+            mav.append(np.mean(np.abs(dados)))
+            rms.append(np.sqrt(np.mean(dados ** 2)))
+            zcs.append(np.where(np.diff(np.sign(dados)))[0].shape[0])
+            scc.append(np.count_nonzero(np.diff(np.sign(np.diff(dados))) != 0))
+            wal.append(np.sum(np.abs(np.diff(dados))))
+
+            var.append(np.sum((dados-np.mean(dados))**2)/(dados.shape[0]-1))
+
+            fft_result = np.fft.fft(dados)
+            spectre = np.abs(fft_result)
+            positive_spectre = spectre[:len(spectre) // 2]
+            fmn.append(np.mean(positive_spectre))
+            fmd.append(np.median(positive_spectre))
+    
+
         
